967538cb71851a931f60a708a67cf957464fd230
Leo5661 <singhra66@gmail.com>
2019-10-13 20:08:31 +0530

    initial

--- app/src/main/java/com/example/first/Main2Activity.java
+++ app/src/main/java/com/example/first/Main2Activity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class Main2Activity extends AppCompatActivity {
 
+    String TAG = "Log";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
